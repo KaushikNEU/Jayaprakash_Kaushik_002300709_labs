20aae2159c9739558a3a5fe84268aff94f19be92
KaushikNEU <lnu.kau@northeastern.edu>
2024-11-10 13:33:21 -0500

    Commit 1- Implementing as per Skeleton Code

---
+++ Lab6/src/model/MasterOrderList.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kaushikj
+ */
+public class MasterOrderList {
+    
+    ArrayList<Order> orderList;
+    private final ArrayList<Order> orderlist;
+
+    public MasterOrderList() {
+        
+    orderlist = new ArrayList<Order>();
+    }
+
+    public ArrayList<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    public void addNewOrder(Order currentOrder) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
